Fix evaluation for causal encoder

evals/video_classification_frozen/eval.py
@@ -527,7 +527,7 @@ class AttnMaskCollator:
 
             videos[i, :, :t] = x[0][0][0]
             patches_num = t_patches*H_patches*W_patches
-            frame_mask[i, :patches_num, :patches_num] = True
+            frame_mask[i, :, :patches_num, :patches_num] = True
 
         # clip_indices = torch.utils.data.default_collate([sample[2] for sample in batch])
         labels = torch.utils.data.default_collate([list(map(int, sample[1].split(','))) for sample in batch]) # Multi-labels should be separated by a comma

src/datasets/video_dataset.py
@@ -352,7 +352,7 @@ class VideoDataset(torch.utils.data.Dataset):
                 indices = indices + i * partition_len
             else:
                 if self.allow_variable_length:
-                    indices = np.linspace(0, partition_len, num=partition_len // fstp)
+                    indices = np.linspace(0, partition_len, num=max(partition_len // fstp, 1))
                     indices = np.clip(indices, 0, partition_len - 1).astype(np.int64)
                     indices = indices + i * partition_len
 

src/models/utils/modules.py
@@ -388,10 +388,14 @@ class RoPEAttention(nn.Module):
             # attn_mask = attn_mask.reshape(T * H_patches * W_patches, T * H_patches * W_patches)
 
             current_patches_num = grid_depth * self.grid_size * self.grid_size
-            attn_mask = self.temp_attn_mask[:current_patches_num,:current_patches_num]
+            causal_attn_mask = self.temp_attn_mask[:current_patches_num,:current_patches_num]
+            if attn_mask is not None:
+                causal_attn_mask = causal_attn_mask.view((1,) * (attn_mask.dim() - causal_attn_mask.dim()) + causal_attn_mask.shape).to(attn_mask.dtype) # Make the casusal mask have the same number of dims as attn_mask
+                causal_attn_mask = attn_mask * causal_attn_mask
+            
             with torch.backends.cuda.sdp_kernel():
                 x = F.scaled_dot_product_attention(
-                    q, k, v, dropout_p=self.proj_drop_prob, attn_mask=attn_mask
+                    q, k, v, dropout_p=self.proj_drop_prob, attn_mask=causal_attn_mask
                 )
                 attn = None
         else: